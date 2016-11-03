0084ec9f5165b711dc67c79cd8c50e099a8468a7
Jeff George <jeff.george@gmail.com>
2016-11-02 21:23:08 -0400

    Terrible Things to ensure we actually really really have enough data to talk to Bungie

--- vendor-ocd.php
+++ vendor-ocd.php
@@ -21,3 +21,25 @@ require "config.php";
 $config["platform_id"] = ($config["platform"] == "xbox") ? "1" : "2";
 
 do_webauth($config["platform"],$config["platform_user"],$config["platform_pass"]);
+
+// figure out if we've got bungled & bungleatk cookies
+$csrf = false;
+$atk = false;
+$cookies = explode("\n",file_get_contents("cookies.txt"));
+foreach ($cookies as $cookie) {
+  $pieces = explode("\t", $cookie);
+  if (count($pieces) > 1){
+    if ($pieces[5] == "bungled"){
+      $csrf = $pieces[6];
+    }
+    if ($pieces[5] == "bungleatk"){
+      $atk = true;
+    }
+  }
+}
+if ($csrf == false){
+  die("No bungled cookie, check your credentials");
+}
+if ($atk == false){
+  die("No bungleatk cookie, check your XBL/PSN credentials");
+}
