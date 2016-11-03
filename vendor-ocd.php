e15728d8d60a9434c921b69a94172f496f35eca9
Jeff George <jeff.george@gmail.com>
2016-11-02 21:24:11 -0400

    cURL Boilerplate

--- vendor-ocd.php
+++ vendor-ocd.php
@@ -43,3 +43,18 @@ if ($csrf == false){
 if ($atk == false){
   die("No bungleatk cookie, check your XBL/PSN credentials");
 }
+
+$user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1";
+$default_options = array(
+  CURLOPT_USERAGENT => $user_agent,
+  CURLOPT_COOKIEJAR => $cookie_file,
+  CURLOPT_COOKIEFILE => $cookie_file,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_SSL_VERIFYHOST => 2,
+  CURLOPT_SSL_VERIFYPEER => false,
+  CURLOPT_HTTPHEADER => array(
+    'X-API-Key: '.$config['apikey'],
+    'X-CSRF: '.$csrf,
+  ),
+);
+
